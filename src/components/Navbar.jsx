7317a24d7cd0c0f8ec67d98902474a26b9c5eba2
Darshan <darshanbitla.iitb@gmail.com>
2024-03-24 03:34:31 +0530

    Added dropdown functionality to mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import "./styles/Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logo from "./images/ai-logo.webp";
+import menu from "./images/ham.webp";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <nav id="pc-nav" className="Navbar">
@@ -26,18 +34,27 @@ function Navbar() {
         </ul>
       </nav>
       <nav id="mobile-nav">
-        <img alt="logo image" src={logo} className="logo-img"></img>
-        <div className="Links">
+        <img alt="logo image" src={logo} className="logo-img" />
+        <img
+          src={menu}
+          alt="menu-icon"
+          className="menu-img onClick={toggleMenu}"
+          onClick={toggleMenu}
+        />
+      </nav>
+      {isMenuOpen && (
+        <div className={`mobile-menu-dropdown ${isMenuOpen ? "open" : ""}`}>
           <ul>
             <li>
               <Link to="/">HOME</Link>
             </li>
+            <hr />
             <li>
               <Link to="/favorites">FAVORITES</Link>
             </li>
           </ul>
         </div>
-      </nav>
+      )}
     </>
   );
 }
